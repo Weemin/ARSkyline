76156e82adf6c87298198637a31ac6c8ba4277ea
Maxime Premont <maxime.premont@gmail.com>
2022-06-27 17:45:11 +0200

    ADD: Scene Loading system

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+#if UNITY_EDITOR
+    using UnityEditor;
+    using UnityEditor.SceneManagement;
+#endif
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[ExecuteInEditMode]
+public class SceneLoader : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField] private SceneReference[] scenes;
+    public string scenesFolder;
+    public bool wipeScenesList = false;
+
+    public void Start() {
+        if (Application.isEditor) {
+            #if UNITY_EDITOR
+                if (EditorApplication.isPlayingOrWillChangePlaymode) {
+                    for (int j = 0; j < scenes.Length; j++) {
+                        LoadSceneParameters parameters = new LoadSceneParameters();
+                        parameters.loadSceneMode = LoadSceneMode.Additive;
+                        SceneManager.UnloadSceneAsync(scenes[j]);
+                        EditorSceneManager.LoadSceneInPlayMode(scenes[j].ScenePath, parameters);
+                    }
+                } else {
+                    for (int j = 0; j < scenes.Length; j++)
+                        EditorSceneManager.OpenScene(scenes[j].ScenePath, OpenSceneMode.Additive);
+                }
+            #endif
+        } else {
+            for (int j = 0; j < scenes.Length; j++)
+                SceneManager.LoadScene(scenes[j], LoadSceneMode.Additive);
+        }
     }
 }
